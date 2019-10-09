fcd2e914f9502fbba5177e926c700c773f859fe9
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-10-09 21:37:57 +0700

    display_answer_list

--- app/controllers/mini_games_controller.rb
+++ app/controllers/mini_games_controller.rb
@@ -1,4 +1,7 @@
 class MiniGamesController < ApplicationController
   def index
+    @questions = Settings.questions
+    @answers = Settings.answers.map {|answer| answer.split(",")}
+    @index = Settings.index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def answer_char_css answer_char, answer_char_index, index
+    has_text_css = "has-text" unless answer_char == "_"
+    final_answer_css = "final-answer" if answer_char_index == index
+
+    "#{has_text_css} #{final_answer_css}"
+  end
 end
